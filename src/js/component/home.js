07428d4507a92151806d36247165a09c51c63e95
luigui52 <luigui52@gmail.com>
2021-03-24 01:57:15 +0000

    Styles changes applied

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,47 +3,49 @@ import React from "react";
 //create your first component
 export function Home() {
 	return (
-		<div className="container">
-			<h1 className="text-center text-muted">To Do List</h1>
-			<div class="input-group mb-3">
+		<div className="container-fluid">
+			<div className="p-3 mb-2 bg-primary text-white">
+				<h1 className="text-center text-light">Luigi's To Do List</h1>
+			</div>
+			<div className="input-group mb-3">
 				<input
 					type="text"
-					class="form-control"
+					className="form-control"
 					placeholder="¿Que pendientes tengo?"
 					aria-label="¿Que pendientes tengo?"
 					aria-describedby="button-addon2"
 				/>
-				<div class="input-group-append">
+				<div className="input-group-append">
 					<button
-						class="btn btn-outline-secondary"
+						className="btn btn-outline-secondary"
 						type="button"
 						id="button-addon2">
-						Clear
+						Agregar
 					</button>
 				</div>
 			</div>
 
 			<ul className="list-group">
 				<li className="list-group-item font-weight-lighter">
-					Mi primera tarea
+					Mi primer tarea
 					<button className="btn float-right text-muted">
-						<i class="fas fa-user-times"></i>
+						<i className="fas fa-user-times"></i>
 					</button>
 				</li>
 				<li className="list-group-item font-weight-lighter">
 					Mi segunda tarea
 					<button className="btn float-right text-muted">
-						<i class="fas fa-user-times"></i>
+						<i className="fas fa-user-times"></i>
 					</button>
 				</li>
 				<li className="list-group-item font-weight-lighter">
-					Mi tercera tarea
+					Mi tercer tarea
 					<button className="btn float-right text-muted">
-						<i class="fas fa-user-times"></i>
+						<i className="fas fa-user-times"></i>
 					</button>
 				</li>
 			</ul>
 			<p className="text-muted card pl-1">3 items left</p>
 		</div>
 	);
-};
+}
